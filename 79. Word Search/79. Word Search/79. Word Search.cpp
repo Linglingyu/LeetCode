dd2146decfd1f5a76dc4b9153346b8cbee6b1bb1
Cherry <liulingyu5303@vip.qq.com>
2017-11-14 12:25:25 +0800

    fix bug（word search）

--- 79. Word Search/79. Word Search/79. Word Search.cpp
+++ 79. Word Search/79. Word Search/79. Word Search.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 bool exist(vector<vector<char>>& board, int y, int x, string word, int begin){
 	if (begin == word.size())return true;
-	if (y < 0 || x < 0 || y == word.size() || x == word.size()||board[y][x]=='\0')return false;
+	if (y < 0 || x < 0 || y == word.size() || x == word[y].size()||board[y][x]=='\0')return false;
 	if (board[y][x] != word[begin])return false;
 	char t = board[y][x];
 	board[y][x] = '\0';
@@ -36,16 +36,17 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	vector<vector<char>> board;
 	
-	vector<char> innner1{ 'A', 'B', 'C', 'E' };
+	/*vector<char> innner1{ 'A', 'B', 'C', 'E' };
 	vector<char> innner2{ 'S', 'F', 'C', 'S' };
-	vector<char> innner3{ 'A', 'D', 'E', 'E' };
+	vector<char> innner3{ 'A', 'D', 'E', 'E' };*/
 
-	//vector<char> innner1{ 'A' };
+	vector<char> innner1{ 'A' };
+	vector<char> innner2{ 'A' };
 	board.push_back(innner1);
 	board.push_back(innner2);
-	board.push_back(innner3);
+	//board.push_back(innner3);
 
-	bool res = exist(board, "BCF");
+	bool res = exist(board, "AA");
 	system("pause");
 	return 0;
 }
